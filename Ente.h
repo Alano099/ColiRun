246a7649bfdb0128c374a4bf8b7468459c5b24e3
Gabriel Alano <66089109+Alano099@users.noreply.github.com>
2025-05-31 17:20:08 -0300

    Revert "Classe Entidade"

--- Ente.h
+++ Ente.h
@@ -1,19 +1,26 @@
 #pragma once
 
-#include "stdafx/stdafx.h"
-#include "IDs/IDs.h"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include<iostream>
+#include<stdlib.h>
 
 class Ente
 {
 
 	protected:
-		const IDs::IDs ID;
+	sf::RectangleShape rect;
+	sf::Vector2f pos;
 
 	public:
 
-		Ente(const IDs::IDs ID);
-		virtual ~Ente() ;
-		const IDs::IDs getID() const { return ID; }
-		virtual void desenhar() = 0;
+		Ente(sf::Vector2f tam, sf::Vector2f position, sf::Color cor);
+		
+		void mover(float dx, float dy);
+		void setPosition(sf::Vector2f position);
+		void resetarPosicao();
+		void desenhar(sf::RenderWindow& janela);
+		bool intercepta(const Ente& outro) const;
+		sf::Vector2f getPosition() const;
 };
 
