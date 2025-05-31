03a03c292d444bbc03f6236508d2e4505fc95b31
Paulo Victor Souza de Almeida <67125342+PauloV-Almeida@users.noreply.github.com>
2025-05-31 16:51:01 -0300

    GerenciadorGrafico

---
+++ Gerenciador_Grafico.cpp
@@ -0,0 +1,55 @@
+#include "Gerenciador_Grafico.h"
+
+Gerenciador_Grafico* Gerenciador_Grafico::instance = nullptr;
+
+
+Gerenciador_Grafico* Gerenciador_Grafico::get_instance() {
+	if (instance == nullptr) {
+		instance = new Gerenciador_Grafico();
+	}
+	return instance;
+}
+
+Gerenciador_Grafico::Gerenciador_Grafico() {
+	janela = new sf::RenderWindow(sf::VideoMode(800, 600), "Gerenciador Grafico");
+	janela->setFramerateLimit(60);
+}
+
+Gerenciador_Grafico::~Gerenciador_Grafico() {
+	delete janela;
+}
+
+sf::RenderWindow* Gerenciador_Grafico::getJanela() {
+	return janela;
+}
+
+void Gerenciador_Grafico::desenhar(sf::RectangleShape* corpo) {
+	if (abreJanela()) {
+		janela->draw(*corpo);
+	}
+}
+void Gerenciador_Grafico::desenhar(sf::CircleShape* corpo) {
+	if (abreJanela()) {
+		janela->draw(*corpo);
+	}
+}
+
+void Gerenciador_Grafico::mostrar() {
+	if (abreJanela()) {
+		janela->display();
+	}
+}
+
+void Gerenciador_Grafico::limpar() {
+	if (abreJanela()) {
+		janela->clear();
+	}
+}
+
+void Gerenciador_Grafico::fechajanela() {
+	janela->close();
+}
+
+const bool Gerenciador_Grafico::abreJanela() {
+	return (janela->isOpen());
+}
